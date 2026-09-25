ac3d67aaf5d66b6f38151c06cbb8bcd2d4026e45
Ajithkumar-Dev07 <ajith.kumar.2k05@gmail.com>
2026-09-25 13:24:17 +0530

    Added Casting and Exception Handling

---
+++ Batch 11 CSharp/Casting.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch_11_CSharp
+{
+    class Employee
+    {
+        public string name;
+     
+       
+        public void LogIn()
+        {
+            Console.WriteLine($"Employee {name} LogedIn to his System");
+        }
+    }
+    
+    class Manger : Employee
+    {
+  
+        public void Work()
+        {
+            Console.WriteLine($"{name} is working as Manager Position");
+        }
+    }
+
+    class HR : Employee
+    {
+        public void ManagerTask()
+        {
+            Console.WriteLine($" Manager {name}and working as the company");
+        }
+    }
+
+    class Developer : Employee
+    {
+        public void Work()
+        {
+            Console.WriteLine("Developer is creating modules for the project");
+        }
+    }
+
+    internal class Casting
+    {
+        static void Main()
+        {
+            //upcasting
+
+            //Employee obj;
+
+            //obj = new Manger();
+
+            //obj.name = "Saran";
+
+            //obj.LogIn();
+
+            //obj.role = "Manager";
+
+            //obj.Work();
+
+            //obj = new HR();
+
+            Manger Managerobj = new Manger();
+
+            Managerobj.name = "Madhan";
+
+            Employee emp = Managerobj;
+
+
+            emp.LogIn();
+
+            Managerobj.Work();
+
+            Console.WriteLine();
+
+            //DownCasting 
+
+            Employee employee = new Manger();
+
+            Manger manage = (Manger)employee;
+
+            manage.name = "Hari";
+
+            manage.Work();
+
+            if(employee is Developer)
+            {
+                Developer dev = (Developer)employee;
+
+                dev.Work();
+            }
+            else
+            {
+                Console.WriteLine("can't be downcast to developer");
+            }
+
+
+            Developer dev1 = null;
+
+            Console.WriteLine(dev1.name);
+        }
+    }
+}
---
+++ Batch 11 CSharp/ExceptionDemo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch_11_CSharp
+{
+    public class InputOddExcepetion : Exception
+    {
+        public InputOddExcepetion(string message) : base(message)
+        {
+            
+        }
+    }
+    internal class ExceptionDemo
+    {
+        
+        static int Main()
+        {
+            try
+            { 
+                Console.WriteLine("Enter the First No: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the Second No: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+
+                //if(num2 % 2 != 0)
+                //{
+                //    throw new InputOddExcepetion("Given number is Odd value");
+                //}
+                
+
+                double div = num1 / num2;
+
+               
+               
+                Console.WriteLine("Result = "+ div);
+
+                string message = null;
+
+                //if(message == null)
+                //{
+                //    throw new NullReferenceException("Null value is Initilaized");
+                //}
+
+
+                //Console.WriteLine("Length of message: " + message.Length);
+
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Error occurs: "+ex.Message);
+                Console.WriteLine("Type of Excpetion: "+ex.GetType());
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Error occurs: " + ex.Message);
+                Console.WriteLine("Type of Excpetion: " + ex.GetType());
+            }
+            catch(ApplicationException ex)
+            {
+                Console.WriteLine("Error occurs: " + ex.Message);
+                Console.WriteLine("Type of Excpetion: " + ex.GetType());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error occurs: " + ex.Message);
+                Console.WriteLine("Type of Excpetion: " + ex.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("End of the Program....");
+            }
+            return 0;
+        }
+    }
+}
